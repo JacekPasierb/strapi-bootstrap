595d9f2a02e19645002d91821e02eff4dbf43687
Jacek Pasierb <jack_byk@o2.pl>
2024-12-29 20:12:12 +0100

    change plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,8 @@
-export default () => ({});
+module.exports = {
+    "users-permissions": {
+      jwt: {
+        secret: process.env.JWT_SECRET || 'tutaj_wklej_swoj_sekretny_klucz',
+      },
+    },
+  };
+  
