1066130f2eab18f5e43692284c23f07fc79e4ebe
Jacek Pasierb <jack_byk@o2.pl>
2024-12-29 20:49:26 +0100

    set origin

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,20 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://front-strapi.netlify.app/"], // Twoja domena Netlify
+      methods: ["GET", "POST", "PUT", "DELETE"], // Dozwolone metody HTTP
+      headers: "*", // Dozwolone nagłówki
+    },
+  },
+
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
